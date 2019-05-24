cea469a4759f179b6c963e06161c1b6135cc7632
Ayoub Benyahya <Ayoub.benyahya@nokia-bell-labs.com>
2019-05-24 10:24:46 +0200

    Adding sdk test android app

---
+++ android_sdk_test/app/src/main/java/mip/belllabs/sdk_test/TestActivity.java
@@ -0,0 +1,36 @@
+package mip.belllabs.sdk_test;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import mip.belllabs.moveinsaclay.OnboardingActivity;
+
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent myIntent = new Intent(TestActivity.this, OnboardingActivity.class);
+                TestActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+
+}
