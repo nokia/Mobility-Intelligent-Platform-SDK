d7ca6d43557fca2af7a352dd90407f3822086253
Ayoub Benyahya <Ayoub.benyahya@nokia-bell-labs.com>
2020-04-10 14:52:05 +0200

    Update MIP SDK V1

--- android_sdk_test/app/src/main/java/mip/belllabs/sdk_test/TestActivity.java
+++ android_sdk_test/app/src/main/java/mip/belllabs/sdk_test/TestActivity.java
@@ -1,36 +1,21 @@
 package mip.belllabs.sdk_test;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import mip.belllabs.moveinsaclay.OnboardingActivity;
+import mip.belllabs.moveinsaclay.HubActivity;
+import mip.belllabs.moveinsaclay.MIS_SDK.AuthManager;
 
-import android.view.View;
-
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Intent myIntent = new Intent(TestActivity.this, OnboardingActivity.class);
-                TestActivity.this.startActivity(myIntent);
-            }
-        });
+        fab.setOnClickListener(view -> AuthManager.init_Auth(TestActivity.this, TestActivity.this, HubActivity.class));
     }
 
 }
